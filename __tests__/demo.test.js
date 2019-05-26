6163763d7ea759297cc81398dc5bf3cb84608f42
frankfang <frankfang1990@gmail.com>
2019-05-26 14:41:58 +0800

    init

---
+++ __tests__/demo.test.js
@@ -0,0 +1,5 @@
+var add = require('../demo.js');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
---
+++ jest.config.js
@@ -0,0 +1,110 @@
+
+module.exports = {
+  // automock: false,
+  // bail: 0,
+  // browser: false,
+  // cacheDirectory: "",
+  // clearMocks: false,
+  // collectCoverage: false,
+  // collectCoverageFrom: null,
+  // coverageDirectory: null,
+  // coveragePathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+  // coverageThreshold: null,
+  // dependencyExtractor: null,
+  // errorOnDeprecated: false,
+  // forceCoverageMatch: [],
+  // globalSetup: null,
+  // globalTeardown: null,
+  // globals: {},
+  // moduleDirectories: [
+  //   "node_modules"
+  // ],
+  // moduleFileExtensions: [
+  //   "js",
+  //   "json",
+  //   "jsx",
+  //   "ts",
+  //   "tsx",
+  //   "node"
+  // ],
+  // moduleNameMapper: {},
+  // modulePathIgnorePatterns: [],
+  // notify: false,
+  // notifyMode: "failure-change",
+
+  // preset: null,
+
+  // projects: null,
+
+  // reporters: undefined,
+
+  // resetMocks: false,
+
+  // resetModules: false,
+
+  // resolver: null,
+
+  // restoreMocks: false,
+
+  // rootDir: null,
+
+  // roots: [
+  //   "<rootDir>"
+  // ],
+
+  // runner: "jest-runner",
+
+  // setupFiles: [],
+
+  // setupFilesAfterEnv: [],
+
+  // snapshotSerializers: [],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // testEnvironmentOptions: {},
+
+  // testLocationInResults: false,
+
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+
+  // testPathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
+
+  // testRegex: [],
+
+  // testResultsProcessor: null,
+
+  // testRunner: "jasmine2",
+
+  // testURL: "http://localhost",
+
+  // timers: "real",
+
+  // transform: null,
+
+  // transformIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
+
+  // unmockedModulePathPatterns: undefined,
+
+  // verbose: null,
+
+  // watchPathIgnorePatterns: [],
+
+  // watchman: true,
+};
